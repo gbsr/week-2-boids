42a51b24078f3b2b70c93f9a8dfe19a47989ab9b
gbsr <anders.jakobsen.hofsten@gmail.com>
2025-11-10 13:42:32 +0100

    config

---
+++ src/config/config.ts
@@ -0,0 +1,21 @@
+export const canvasConfig = {
+  width: 800,
+  height: 600,
+  backgroundColor: '#000000',
+  border: '1px solid #333333',
+  borderRadius: '30px',
+  boxShadow: '0 0 20px rgba(0, 0, 0, 0.5)',
+}
+
+export const boidConfig = {
+  boidCount: 100,
+  perceptionRadius: 50,
+  separationWeight: 1.5,
+  alignmentWeight: 1.0,
+  cohesionWeight: 1.0,
+  maxSpeed: 4,
+  maxForce: 0.1,
+  wrapEdges: true,
+  color: '#FFFFFF',
+}
+
