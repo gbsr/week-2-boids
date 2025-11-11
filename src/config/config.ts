d2d7ab57f6389337e37837d818b36c45881766cd
gbsr <anders.jakobsen.hofsten@gmail.com>
2025-11-11 16:31:38 +0100

    added config stuff for future implementation

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ export const canvasConfig = {
 }
 
 export const boidConfig = {
-  boidCount: 10,
+  boidCount: 100,
   perceptionRadius: 50,
   separationWeight: 1.5,
   alignmentWeight: 1.0,
@@ -13,6 +13,25 @@ export const boidConfig = {
   maxSpeed: 0.35,
   maxForce: 0.1,
   wrapEdges: false,
-  color: '#FFFFFF',
+
+  visualizePerception: false,
+  perceptionColor: '#00ff00',
+  perceptionLineWidth: 1,
+  perceptionAlpha: 0.2,
+
+  visualizeCohesionRadius: false,
+  cohesionRadiusColor: '#0000ff',
+  cohesionRadiusLineWidth: 1,
+  cohesionRadiusAlpha: 0.2,
+
+  visualizeAlignmentRadius: false,
+  alignmentRadiusColor: '#ffff00',
+  alignmentRadiusLineWidth: 1,
+  alignmentRadiusAlpha: 0.2,
+  
+  visualizeSeparationRadius: false,
+  separationRadiusColor: '#ff0000',
+  separationRadiusLineWidth: 1,
+  separationRadiusAlpha: 0.2,
 }
 
